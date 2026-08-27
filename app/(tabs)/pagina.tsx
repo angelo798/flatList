24d60eaad0e15a92b05ce373462386296e3bba44
angelo <angelo.colombo@alunos.sc.senac.br>
2026-08-27 14:15:18 -0300

    pagina finalisada

--- app/(tabs)/pagina.tsx
+++ app/(tabs)/pagina.tsx
@@ -1,21 +1,48 @@
-import {View, StyleSheet,Image,Text} from 'react-native';
-
+import { View, StyleSheet, Image, Text } from 'react-native';
 
 export default function App() {
-    return (
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://tse3.mm.bing.net/th/id/OIP.KRPlsYGTtvCevwGRezsc5wHaKC?r=0&w=620&h=840&rs=1&pid=ImgDetMain&o=7&rm=3"
+        }}
+        style={styles.imagem}
+      />
 
-        <View>
-          <Image source={{ uri: "https://tse3.mm.bing.net/th/id/OIP.KRPlsYGTtvCevwGRezsc5wHaKC?r=0&w=620&h=840&rs=1&pid=ImgDetMain&o=7&rm=3" }} style={{ width: 120, height: 170, borderRadius: 6 }} />
-          <Text>
-           Inspirado nos arquivos reais do Padre Gabriele Amorth, Chefe Exorcista do Vaticano. O padre realizou mais de 100.000 exorcismos em sua vida e faleceu em 2016 aos 91 anos. Amorth escreveu duas memórias – An Exorcist Tells His Story e An Exorcist: More Stories – e detalhou suas experiências lutando contra Satanás e demônios que agarraram e possuíram as pessoas com seu mal. O filme, sendo o retrato do personagem da vida real, acompanha Amorth (Russell Crowe) enquanto ele investiga a terrível possessão de um menino e acaba descobrindo uma conspiração secular que o Vaticano tentou desesperadamente proteger e manter no esquecimento.
-          </Text>
-        </View>
-    );
+      <Text style={styles.texto}>
+        Inspirado nos arquivos reais do Padre Gabriele Amorth, Chefe Exorcista
+        do Vaticano. O padre realizou mais de 100.000 exorcismos em sua vida e
+        faleceu em 2016 aos 91 anos. Amorth escreveu duas memórias – An Exorcist
+        Tells His Story e An Exorcist: More Stories – e detalhou suas
+        experiências lutando contra Satanás e demônios que agarraram e
+        possuíram as pessoas com seu mal.
+        O filme, sendo o retrato do personagem da vida real,
+         acompanha Amorth (Russell Crowe) enquanto ele investiga a terrível
+          possessão de um menino e acaba descobrindo uma conspiração secular que o 
+          Vaticano tentou desesperadamente proteger e manter no esquecimento.
+      </Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({    
-    badgeTexto: {
-    color: "#ffffff",
-    fontSize: 9,
-    fontWeight: "700",
-}});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    padding: 10,
+  },
+
+  imagem: {
+    width: 450,
+    height: 560,
+    borderRadius: 25,
+  },
+
+  texto: {
+    color: '#ffffff',
+    fontSize: 15,
+    marginTop: 12,
+    lineHeight: 20,
+  },
+});
